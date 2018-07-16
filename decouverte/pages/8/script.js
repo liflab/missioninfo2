3ab731f5d229b3257b2fd86f4ca4b148b5b3a932
Benjamin Quessy <benjamin.quessy1@uqac.ca>
2018-07-16 16:20:24 -0400

    added découverte pages  6-7-8

---
+++ decouverte/pages/8/script.js
@@ -0,0 +1,27 @@
+popupInfo("Fais avancer Robotino jusqu’à la case verte \n en utilisant le nouveau bloc : la boucle. \n **Attention, tu n’as le droit d’utiliser que deux blocs.**");
+
+page_map = [
+    {row: 3, col: 2, data: {style: "deb", type: 1}},
+    {row: 3, col: 3, data: {style: "1", type: 1}},
+    {row: 3, col: 4, data: {style: "1", type: 1}},
+    {row: 3, col: 5, data: {style: "2", type: 2}},
+    {row: 4, col: 5, data: {style: "1", type: 0}},
+    {row: 5, col: 5, data: {style: "2", type: 0}},
+    {row: 5, col: 6, data: {style: "1", type: 1}},
+    {row: 5, col: 7, data: {style: "fin", type: 3}}
+];
+
+function allLoaded() {  // Changer et mettre a jour le bon nombre de progressions dans l'activité (modifié 2018-07-16)
+    createButtons(9);
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("page").style.display = "block";
+    autoResize();
+}
+
+function setup() {
+    var canvas = createCanvas(600, 600);
+    canvas.parent('sketch-holder');
+    noLoop();
+
+    createMap();
+}
