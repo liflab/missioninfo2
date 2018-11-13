906b4ecefd99e3ac4f7d56e4022c5d4872b2553e
Simon Gilbert <simon.gilbert1@uqac.ca>
2018-11-13 13:05:11 -0500

    J'ai fait quelques modifications demandées par Sylvain. Regarde Trello.

--- decouverte/pages/4/script.js
+++ decouverte/pages/4/script.js
@@ -1,5 +1,4 @@
 popupInfo("Pour cette progression, nous allons apprendre à utiliser l'aide. \n L'aide sera toujours disponible si jamais tu es bloqué. Si l'aide ne t'aide \n pas à résoudre ton problème, va demander à un coéquipier ou à ton \nenseignant. Utilise le bouton \"Aide\", en haut à gauche, pour afficher l'aide.")
-
 // To check the answer
 var stringAnswer;
 
@@ -8,8 +7,11 @@ function initAnswer() {
 }
 
 function checkAnswer() {
-    if(stringAnswer === "CHIEN") {
+    if(stringAnswer === "CHIEN" && compteura === 1) {
         enable_next();
+    }
+    else if (compteura === 0) {
+        popupNotGood("Il semblerait que tu n'aies pas ouvert l'aide ! Appuie sur le bouton Aide situé en haut à gauche de la page, à droite du bouton Accueil.");
     }
     else {
         not_good();
